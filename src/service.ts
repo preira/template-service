c9bff0fef248ebe0dec481717dace133fc58a115
preira <pereira.joao@gmail.com>
2020-12-26 12:45:45 +0000

    minor tweaks

--- src/service.ts
+++ src/service.ts
@@ -1,8 +1,8 @@
 // use microsware has framework to share repetitive implementation
 
-import { MSWService, Service } from 'microsware'
+import { MSWService, Service } from 'microsware/lib'
 // TODO:
 // set up routes
 
-const server : Service = new MSWService({ server : { httpport : 3000 } })
+const server : Service = new MSWService ({ server : { httpport : 3000 } })
 server.run()
